b3c8f4d1aa7ac81b9f31717e005724223359bd78
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-03-23 15:32:37 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
-     <footer> This project was coded by Sylwia Lis-Mila and is{""} 
-       <a href="https://github.com/SylwiaLis-Mila/react-weather-app" target="_blank"> open-sourced on GitHub </a></footer>
-  
+      <div className="container">
+        <h1>Weather App</h1> 
+            <footer> This project was coded by Sylwia Lis-Mila and is{""} 
+               <a href="https://github.com/SylwiaLis-Mila/react-weather-app" target="_blank"> open-sourced on GitHub </a>
+            </footer>
+      </div>
     </div>
   );
 }
