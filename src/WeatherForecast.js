a69e330ee19833c342f89a23aba97f11ab533542
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-05-14 16:25:00 +0200

    add forecast 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,12 +16,18 @@ export default function WeatherForecast(props) {
        return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">
-      <WeatherForecastDay data={forecast[0]} />
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
-    </div>
-  );
+    );
     
  }else{
    let apiKey = "f58a3da8d9e0f160ba2b997349a49f23";
