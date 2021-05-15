641a1038e8a8e2e3a5f2b30b20b3741c3b17b166
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-05-15 12:40:01 +0200

    add fix forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect }  from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -7,15 +7,29 @@ export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
+   useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+  function load () {
+    let apiKey = "f58a3da8d9e0f160ba2b997349a49f23";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+ 
+     axios.get(apiUrl).then(handleResponse);
+  }
+
  if (loaded) {
        return (
-    <div className="WeatherForecast">
-      <div className="row">
+     <div className="WeatherForecast">
+        <div className="row">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
@@ -23,21 +37,16 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
-    
- }else{
-   let apiKey = "f58a3da8d9e0f160ba2b997349a49f23";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
- 
-     axios.get(apiUrl).then(handleResponse);
-    
-  return null;
+  } else {
+    load();
 
-    }
+    return null;
+  }
 }
