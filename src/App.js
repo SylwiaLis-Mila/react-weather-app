4cce65fff924a602c664ba10dfc6583beda7c162
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-05-16 13:01:12 +0200

    add netlify

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Weather defaultCity="Stuttgart"/>
             <footer> This project was coded by Sylwia Lis-Mila and is{""} 
                <a href="https://github.com/SylwiaLis-Mila/react-weather-app" target="_blank"> open-sourced on GitHub </a>
+               and <a href="https://determined-saha-57968b.netlify.app/">hosted on Netlify</a>
             </footer>
       </div>
     </div>
